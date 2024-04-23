a339cebd20b182445e80f6aca7698b57c3267e15
Till <Till.Seifert@d-fine.de>
2024-04-23 17:05:46 +0100

    remove duplications

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,15 +25,11 @@ class GildedRose {
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                        increaseQualityIfLessThan50(item);
                     }
 
                     if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                        increaseQualityIfLessThan50(item);
                     }
                 }
             }
@@ -55,10 +51,14 @@ class GildedRose {
                     item.quality = 0;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                increaseQualityIfLessThan50(item);
             }
+        }
+    }
+
+    private static void increaseQualityIfLessThan50(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
